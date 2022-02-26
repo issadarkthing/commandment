8ab315960c0ce98c7f22ba005e8d785eb1b36a00
raziman <raziman.terra@gmail.com>
2022-02-26 21:30:22 +0800

    remove release

--- src/CommandManager.ts
+++ src/CommandManager.ts
@@ -61,12 +61,6 @@ export class CommandManager {
     this.verbose && console.log(...values);
   }
 
-  /** Release user from command blocking. Use this format for id:
-   * `commandName_userID` */
-  release(id: string) {
-    this.blockList.get(id)?.cancel();
-  }
-
   /**
    * Register a singular command
    * @param {string} name - The name of the command
