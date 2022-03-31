cb1578fc544ced4cecddde3d222006f983978133
raziman <raziman@tutanota.com>
2022-03-31 15:32:49 +0800

    add help command

---
+++ src/structure/HelpCommand.ts
@@ -0,0 +1,37 @@
+import { Command } from "../index";
+import { Message, MessageEmbed } from "discord.js";
+
+export default class Help extends Command {
+  name = "help";
+  aliases = ["h"];
+  description = "show all commands and it's description";
+
+  async exec(msg: Message) {
+    const commands = this.commandManager.commands.values();
+
+    let helpText = "";
+    const done = new Set<string>();
+
+    for (const command of commands) {
+
+      if (command.disable)
+        continue;
+
+      if (done.has(command.name)) {
+        continue
+      } else {
+        done.add(command.name);
+      }
+
+      helpText += 
+        `\n**${command.name}**: \`${command.description || "none"}\``;
+
+    }
+
+    const embed = new MessageEmbed()
+      .setTitle("Help")
+      .setDescription(helpText)
+
+    this.sendEmbed(msg, embed);
+  }
+}
